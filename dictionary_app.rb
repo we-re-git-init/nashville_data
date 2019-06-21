0c835dc8e7f6f85301e2f233829207c1a81c455a
Brian <riceb53@gmail.com>
2019-06-20 21:14:06 -0500

    show all pronunciations and definitions

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,4 +1,5 @@
 require 'http'
+require 'awesome_print'
 
 # The program should ask the user to enter a word, then use the wordnik API to show the word’s definition: https://developer.wordnik.com/docs#!/word/getDefinitions
 
@@ -6,30 +7,52 @@ require 'http'
 # Bonus: Write your program in a loop such that the user can keep entering new words without having to restart the program. Give them the option of entering q to quit.
 # Bonus: Search for the audio section in the documentation. Use wordnik’s audio api to get the first fileUrl to open in the browser (you’ll need to google this!) and pronounce the name of the word.
 
+p "welcome to the dictionary app, please type your word below"
+user_input = gets.chomp
 
 # definitions
-response = HTTP.get("https://api.wordnik.com/v4/word.json/hippo/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 
-definition = response.parse[0]['text']
+definitions = response.parse
 
+p "printing out the definitions"
+
+definitions.each do |definition|
+  p definition['text']
+end
 
 # top example
-response = HTTP.get("https://api.wordnik.com/v4/word.json/hippo/topExample?useCanonical=false&api_key=")
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/topExample?useCanonical=false&api_key=")
 
 top_example = response.parse['text']
 
-
-# pronunciation
-response = HTTP.get("https://api.wordnik.com/v4/word.json/hippo/pronunciations?useCanonical=false&limit=50&api_key=")
-
-pronunciation = response.parse[0]['raw']
-
-
-p "definitions:"
-p definition
-
-p "top example"
+p "printing out the top example"
 p top_example
 
-p "pronunciation:"
-p pronunciation
+# # pronunciation
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/pronunciations?useCanonical=false&limit=50&api_key=")
+
+pronunciations = response.parse
+
+p "printing out the pronunciations"
+pronunciations.each do |pronunciation|
+  p pronunciation['raw']
+end
+
+
+
+
+# p "definitions:"
+# p definition
+
+# p "top example"
+# p top_example
+
+# p "pronunciation:"
+# p pronunciation
+
+
+
+# # no user input DONE
+# # only showing one definition
+# # user should be able to enter multiple words without exiting the program
